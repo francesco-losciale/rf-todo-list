a009d9766c09a84a0461098e3df7f14a266529ee
Francesco Losciale <francesco.losciale@thoughtworks.com>
2021-05-02 16:05:30 +0100

    use reagent atom to keep value of input text

--- src/rf_todo_list/views.cljs
+++ src/rf_todo_list/views.cljs
@@ -1,22 +1,27 @@
 (ns rf-todo-list.views
   (:require
+    [reagent.core :as re-agent]
     [re-frame.core :as re-frame]
     [rf-todo-list.subs :as subs]
     ))
 
 (defn main-panel []
-  (let [todo-list @(re-frame/subscribe [:todo-list-load])]
+  (let [todo-list @(re-frame/subscribe [:todo-list-load])
+        value (re-agent/atom nil)]
     [:div
      [:ul
       (for [item todo-list] ^{:key (:id item)}
                             [:li [item :description]])
       ]
      [:form
-      [:input#description {:name "description" :type "text"}]
+      [:input {:name    "description"
+               :type    "text"
+               :on-change #(reset! value (-> % .-target .-value))
+               }]
       [:button
-             {:type "button"
-              :on-click
-                    (fn [_]
-                      (re-frame/dispatch [:todo-list-add-item
-                                          {:id 3 :description "Item 3"}]))}
-             "Add"]]]))
+       {:type "button"
+        :on-click
+              (fn [_]
+                (re-frame/dispatch [:todo-list-add-item
+                                    {:id 3 :description @value}]))}
+       "Add"]]]))
