6c7864d576708106ca1f2a19ffd2e534ee2c0a01
Francesco Losciale <francesco.losciale@thoughtworks.com>
2021-05-02 11:51:29 +0100

    add project.clj file so that you can use repl with shadow-cljs

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject rf-todo-list "0.0.1"
+  :dependencies
+  [[thheller/shadow-cljs "2.11.14" :scope "provided"]
+   [reagent "1.0.0"]
+   [re-frame "1.2.0"]
+
+   [binaryage/devtools "1.0.3"]]
+
+  :source-paths
+  ["src"])
