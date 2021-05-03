f3289b51bc93be1663f239016bb0e00f0c0d4b1c
Francesco Losciale <francesco.losciale@thoughtworks.com>
2021-05-03 10:10:14 +0100

    create next-item-id interceptor

--- src/rf_todo_list/events.cljs
+++ src/rf_todo_list/events.cljs
@@ -15,8 +15,19 @@
     (re-frame/dispatch [:todo-list-load])
     ))
 
+(def add-next-item-id
+  (re-frame/->interceptor
+    :id :next-item-id
+    :before (fn [context]
+              (let [db (:db (:coeffects context))
+                    todo-list (:todo-list db)
+                    new-item-id ((fnil inc 0) (:id (last todo-list)))]
+                (assoc-in context [:coeffects :next-item-id] new-item-id)
+                ))))
+
 (re-frame/reg-event-db
   :todo-list-add-item
+  [add-next-item-id]
   (fn [db event]
     (let [item (second event)]
       (update db :todo-list conj item))))
