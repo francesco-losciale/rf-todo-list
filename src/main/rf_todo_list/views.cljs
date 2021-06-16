17f3da955191d9a8a3c50d12d3900f1b7d4071e1
Francesco Losciale <francesco.losciale@thoughtworks.com>
2021-06-16 19:35:48 +0100

    if you don't use `re-agent/with-let` instead of `let`, the atom will be recreated every time the component is re-rendered. you should extract the atom.

--- src/main/rf_todo_list/views.cljs
+++ src/main/rf_todo_list/views.cljs
@@ -6,8 +6,7 @@
     ))
 
 (defn main-panel []
-  (let [todo-list @(re-frame/subscribe [:todo-list-load :todo-list-save])
-        value (re-agent/atom nil)]
+  (let [todo-list @(re-frame/subscribe [:todo-list-load :todo-list-save])]
     [:div
      [:ul
       (for [item todo-list] ^{:key (:id item)}
@@ -23,23 +22,26 @@
                                            (:id item)]))}
                                " - "]]])
       ]
-     [:form
-      [:input {:name      "description"
-               :type      "text"
-               :on-change #(reset! value (-> % .-target .-value))
-               }]
-      [:button
-       {:type "button"
-        :on-click
-              (fn [_]
-                (if (not (nil? @value))
-                  (re-frame/dispatch [:todo-list-add-item
-                                      {:text @value}])))}
-       "Add"]
-      [:button
-       {:type "button"
-        :on-click
-              (fn [_]
-                (re-frame/dispatch [:todo-list-save
-                                    { :todo-list todo-list}]))}
-       "Save"]]]))
+     (re-agent/with-let [draft (re-agent/atom nil)
+                         value (re-agent/track #(or @draft ""))]
+       [:div
+        [:input {:type      "text"
+                 :on-change #(reset! draft (.. % -target -value))
+                 :value     @value
+                 }]
+        [:button
+         {:type "button"
+          :on-click
+                (fn [_]
+                  (if (not (nil? @value))
+                    (re-frame/dispatch [:todo-list-add-item
+                                        {:text @value}]))
+                  (reset! draft ""))}
+         "Add"]
+        [:button
+         {:type "button"
+          :on-click
+                (fn [_]
+                  (re-frame/dispatch [:todo-list-save
+                                      {:todo-list todo-list}]))}
+         "Save"]])]))
